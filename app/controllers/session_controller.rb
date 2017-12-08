ce3d11c00c144211e2935711c07f1615652c6abf
leducduy <duyanh.3895@gmail.com>
2017-12-08 10:49:36 +0700

    Update-form_signup

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,7 +8,7 @@ class SessionController < ApplicationController
       log_in user
       remember_params user
       flash[:success] = t "flash.login_success"
-      redirect_back_or user
+      redirect_back_or user_path
     else
       flash.now[:danger] = t "flash.user_invalid"
       render :sign_in
